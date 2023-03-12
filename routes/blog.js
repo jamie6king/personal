75740738532702bdb013207b1e5148057200e4d6
jamie6king <git6king@proton.me>
2023-03-12 12:14:10 +0000

    Created blog backend
    Signed-off-by: jamie6king <git6king@proton.me>

--- routes/blog.js
+++ routes/blog.js
@@ -7,15 +7,39 @@ JS for the blog
 const express = require("express")
 const router = express.Router()
 
+// Setup fs
+const fs = require("fs")
+
 // GET homepage
 router.get("/", (req, res) => {
 
+    var files = []
+    var posts = []
+
+    try {
+
+        // Get files
+        fs.readdirSync("./public/blogs/").forEach(file => {
+            files.push(file)
+        })
+
+        // Get file data
+
+        for (i = 0; i < files.length; i++) {
+            let data = fs.readFileSync("./public/blogs/" + files[i], "utf-8").split("\n")
+            let post = { title: data[0], date: data[1], link: "/blog/" + files[i] }
+            posts.push(post)
+        }
+    } catch(err) {
+    }
+
     // Format: { title: "", date: "", link: "" }
-    res.render("blog-homepage", { blog_posts: [ ] })
+    res.render("blog-homepage", { blog_posts: posts })
 })
 
 // GET article
-router.get("/:year/:month/:day/:title", (req, res) => {
+router.get("/:post", (req, res) => {
+
     res.render("blog")
 })
 
